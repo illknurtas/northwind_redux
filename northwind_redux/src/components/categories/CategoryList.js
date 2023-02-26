a67aec8c8351f331d246401e953b4dfddcf0277a
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-26 17:28:15 +0300

    error update

--- northwind_redux/src/components/categories/CategoryList.js
+++ northwind_redux/src/components/categories/CategoryList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { connect} from 'react-redux'
 
-export default class CategoryList extends Component {
+class CategoryList extends Component {
   render() {
     return (
       <div>
         <h1>categories</h1>
+        <h5>Selected Category: {this.props.currentCategory}</h5>
       </div>
     )
   }
 }
+function mapStateToProps(state) {
+  return {
+    currentCategory: state.changeCategoryReducer
+  }
+}
+
+export default connect(mapStateToProps)(CategoryList);
