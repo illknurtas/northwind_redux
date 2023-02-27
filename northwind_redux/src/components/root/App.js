71540857e00a9f915e5ee154c361a9d92aa0f8ba
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-27 16:56:26 +0300

    routing

--- northwind_redux/src/components/root/App.js
+++ northwind_redux/src/components/root/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import {Container} from "reactstrap";
 import Navi from "../navi/Navi";
 import Dashboard from "./Dashboard";
+import {Routes, Route} from "react-router-dom";
+import CartDetail from "../cart/CartDetail";
 
 function App() {
   return (
     <Container>
       <Navi/>
-      <Dashboard/>
+      <Routes>
+        <Route path="/" exact element={<Dashboard/>}/>
+        <Route path="/product" exact element={<Dashboard/>}/>
+        <Route path="/cart" exact element={<CartDetail/>}/>
+      </Routes>
     </Container>
   );
 }
