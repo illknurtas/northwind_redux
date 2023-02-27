60558719b0a75a9a9754642d499d1c3abdbf0e90
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-28 01:49:26 +0300

    select input update

--- northwind_redux/src/components/products/ProductDetail.js
+++ northwind_redux/src/components/products/ProductDetail.js
@@ -1,7 +1,8 @@
 import React from "react";
+import SelectInput from "../toolbox/SelectInput";
 import TextInput from "../toolbox/TextInput";
 
-const ProductDetail =( categories, product, onSave,  onChange)=>{
+const ProductDetail =( {categories, product, onSave,  onChange})=>{
     return(
         <form onSubmit={onSave}>
             <h2>
@@ -13,6 +14,21 @@ const ProductDetail =( categories, product, onSave,  onChange)=>{
             value ={product.productName}
             onChange={onChange}
             error="Error"/>
+
+            <SelectInput 
+            name="categoryId"
+            label="Category"
+            value = {product.categoryId || ""}
+            defaultOption ="Select"
+            options={categories.map(
+                category=>({
+                    value: category.id,
+                    text: category.categoryName
+                })
+            )}
+            onChange={onChange}
+            error="Error"/>
+
             <button type="submit"
             className="btn btn-success">Save</button>
         </form>
