d4bff4c8a0a0fc32619116e3d417d3bf9556e2e3
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-27 16:46:25 +0300

    remove from cart

--- northwind_redux/src/components/cart/CartSummary.js
+++ northwind_redux/src/components/cart/CartSummary.js
@@ -8,6 +8,9 @@ import {
     NavLink,
     Badge} from 'reactstrap';
 import {connect} from "react-redux";
+import { bindActionCreators } from 'redux';
+import * as cartActions from "../../redux/actions/cartActions";
+
 
 class CartSummary extends Component {
     renderEmpty(){
@@ -30,6 +33,11 @@ class CartSummary extends Component {
                         this.props.cart.map( 
                             cartItem =>(
                                 <DropdownItem key={cartItem.product.id}>
+                                    <Badge 
+                                    color="danger"
+                                    onClick={()=>this.props.actions.removeFromCart(cartItem.product)}>
+                                        X
+                                    </Badge>
                                     {cartItem.product.productName}
                                     <Badge color='success'>
                                         {cartItem.quantity}
@@ -64,4 +72,13 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(CartSummary);
+function mapDispatchToProps(dispatch) {
+    return{
+        actions:{
+            removeFromCart: bindActionCreators(cartActions.removeFromCart, dispatch),
+
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartSummary);
